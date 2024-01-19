61cf23fc1e428ba57fc611731f8fad7314b9d3c7
nandanpi <nandanrp09@gmail.com>
2024-01-19 23:02:44 +0530

    About the project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,47 @@
 <div class="bg-[#fad6ca] text-[#2a4fb2]">
-	<div class="flex justify-between fixed w-full py-8 md:px-16 px-4 font-semibold">
-		<h3 class="md:text-2xl text-xl">Team KFC</h3>
-		<h3 class="md:text-2xl text-xl">HackLoop 2024</h3>
+	<div
+		class="flex justify-between fixed w-full py-6 sm:px-8 px-2 font-semibold bg-[#fad6ca] items-center"
+	>
+		<a href="https://finiteloop.co.in" target="_blank">
+			<span class="flex items-center gap-x-2">
+				<img src="/flc_logo_crop.png" alt="flc_Logo" class="md:h-16 h-12" />
+				<h3 class="sm:text-2xl text-md font-bold">Finite Loop Club</h3>
+			</span>
+		</a>
+		<a href="https://www.instagram.com/p/C2ES0MOv7tK/" target="_blank">
+			<h3 class="sm:text-2xl text-md font-bold">HackLoop</h3>
+		</a>
 	</div>
-	<div class="flex justify-center items-center w-full min-h-screen">
-		<h1 class="md:text-7xl text-4xl font-bold">Coming Soon</h1>
+	<div class="flex items-center justify-center w-full min-h-screen pt-32">
+		<div class="w-[80%] justify-center space-y-8">
+			<h1 class="md:text-6xl text-3xl font-bold">About this project</h1>
+
+			<h3 class="md:text-4xl text-2xl font-semibold">Problem Statement</h3>
+			<p class="max-w-4xl text-justify md:text-xl text-md">
+				In today's fast-paced world, staying informed about local events such as accidents, traffic
+				jams, and other incidents is crucial for individuals to navigate their surroundings
+				efficiently. When it comes locally, to a place like Mangalore, to notify people about
+				potholes, random road construction, unmarked speed breakers etc. To address this need,
+				develop a user-friendly solution that allows users to address and check for real-time local
+				events happening in their vicinity.
+			</p>
+
+			<h3 class="md:text-4xl text-2xl font-semibold">Solution</h3>
+			<ul class="max-w-4xl text-justify md:text-xl text-md list-disc ml-10">
+				<li>Feed that shows all local events (accidents/traffic jam etc) in a particular area.</li>
+				<li>Users can also report an event that they have witnessed.</li>
+				<li>Satellite map with verified traffic tags</li>
+			</ul>
+
+			<h3 class="md:text-4xl text-2xl font-semibold">Major Problems to be solved</h3>
+			<ul class="max-w-4xl text-justify md:text-xl text-md list-disc ml-10">
+				<li>
+					Verifying authenticity of users <span class="font-semibold"
+						>(Solution: Using Meri Pehchaan API)</span
+					>
+				</li>
+				<li>Verifying authenticity of posts</li>
+			</ul>
+		</div>
 	</div>
 </div>
